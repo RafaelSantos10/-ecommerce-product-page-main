61e97b86d1634c026f964c77bb04d0a6f845a085
Rafael Santos <faelsant10@gmail.com>
2022-02-18 19:37:32 -0300

    atualizção

--- script.js
+++ script.js
@@ -2,11 +2,16 @@ const addCart = document.querySelector("#add");
 const rmvCart = document.querySelector("#rmv");
 const dispay = document.querySelector("#display");
 let item = 0;
-dispay.innerHTML = item;
-
-addCart.addEventListener("click", addItemCart);
-rmvCart.addEventListener("click", rmvItemCart);
-
+const cards = document.querySelector(".cards_down");
+let indexImg = 0;
+let modal = document.querySelector(".modal");
+const wind = window.window;
+const divCard = document.querySelector(".cards");
+const next = document.querySelector(".next");
+const previous = document.querySelector(".previous");
+const imgPrev = document.querySelector(".imgPrev");
+let prev = document.querySelector(".card_prev");
+let close = document.querySelector(".close");
 const bntAdd = document.querySelector(".addCart");
 const itemThisCart = document.querySelector(".cartItem");
 const empety = document.querySelector(".prod");
@@ -14,14 +19,21 @@ const def = document.querySelector(".default");
 const del = document.querySelector(".delete");
 const cart = document.querySelector(".cart");
 const closeCart = document.querySelector(".openCart");
-
-bntAdd.addEventListener("click", addToCard);
-del.addEventListener("click", delCart);
-cart.addEventListener("click", opentCart);
-
 const times = document.querySelector(".times");
 const total = document.querySelector(".total");
 let calcTot;
+const mobileMenu = document.querySelector(".mobile_menu");
+const menu = document.querySelector(".menu");
+const buttonX = document.querySelector(".close_menu");
+
+dispay.innerHTML = item;
+addCart.addEventListener("click", addItemCart);
+rmvCart.addEventListener("click", rmvItemCart);
+bntAdd.addEventListener("click", addToCard);
+del.addEventListener("click", delCart);
+cart.addEventListener("click", opentCart);
+mobileMenu.addEventListener("click", openMenuMobile);
+buttonX.addEventListener("click", closeMenu);
 
 function addItemCart() {
   item++;
@@ -33,53 +45,46 @@ function rmvItemCart() {
   }
   dispay.innerHTML = item;
 }
-setInterval(function () {
-  const cards = document.querySelector(".cards_down");
-
-  const wind = window.window;
-  const divCard = document.querySelector(".cards");
-
-  let modal = document.querySelector(".modal");
-  let prev = document.querySelector(".card_prev");
-  let close = document.querySelector(".close");
-
+ setInterval(function () {
   close.addEventListener("click", closeModal);
   prev.addEventListener("click", addModal);
   cards.addEventListener("click", selectCards);
   wind.addEventListener("click", closeAll);
+  next.addEventListener("click", nextImg);
+  previous.addEventListener("click", previousImg);
 
   function selectCards(e) {
-    let card = e.target;
-    childCards = this.children;
-    arrayCards = Array.from(childCards);
-    arrayCards.forEach((element, index, out) => {
-      if (element.classList.contains("activeCard") === true) {
-        element.classList.remove("activeCard");
+    if (screen.width >= 768) {
+      let card = e.target;
+      childCards = this.children;
+      arrayCards = Array.from(childCards);
+      arrayCards.forEach((element, index, out) => {
+        if (element.classList.contains("activeCard") === true) {
+          element.classList.remove("activeCard");
+        }
+      });
+
+      if (card.classList.value === "thumbnail1") {
+        imgPrev.src = "./images/image-product-1.jpg";
+      } else if (card.classList.value === "thumbnail2") {
+        imgPrev.src = "./images/image-product-2.jpg";
+      } else if (card.classList.value === "thumbnail3") {
+        imgPrev.src = "./images/image-product-3.jpg";
+      } else if (card.classList.value === "thumbnail4") {
+        imgPrev.src = "./images/image-product-4.jpg";
       }
-    });
 
-    if (card.classList.value === "thumbnail1") {
-      prev.innerHTML =
-        '<img class="imgPrev" src="./images/image-product-1.jpg" alt="" />';
-    } else if (card.classList.value === "thumbnail2") {
-      prev.innerHTML =
-        '<img class="imgPrev" src="./images/image-product-2.jpg" alt="" />';
-    } else if (card.classList.value === "thumbnail3") {
-      prev.innerHTML =
-        '<img class="imgPrev" src="./images/image-product-3.jpg" alt="" />';
-    } else if (card.classList.value === "thumbnail4") {
-      prev.innerHTML =
-        '<img class="imgPrev" src="./images/image-product-4.jpg" alt="" />';
+      card.classList.add("activeCard");
     }
-
-    card.classList.add("activeCard");
   }
 
   function addModal() {
-    modal.innerHTML = divCard.outerHTML;
-    modal.classList.remove("none");
-    modal.classList.add("open");
-    close.classList.remove("none");
+    if (screen.width > 768) {
+      modal.innerHTML = divCard.outerHTML;
+      modal.classList.remove("none");
+      modal.classList.add("open");
+      close.classList.remove("none");
+    }
   }
 
   function closeModal() {
@@ -96,17 +101,45 @@ setInterval(function () {
     ) {
       closeCart.classList.remove("cartOpenOn");
     }
-    if (
-      target.classList[0] !== "imgPrev" &&
-      target.classList[1] !== "activeCard"
-    ) {
-      modal.classList.remove("open");
-      close.classList.add("none");
-      modal.classList.add("none");
-      modal.innerHTML = "";
-    }
+
+    console.log(closeCart.classList)
+    // if (
+    //   target.classList[0] !== "imgPrev" &&
+    //   target.classList[1] !== "activeCard"
+    // ) {
+    //   modal.classList.remove("open");
+    //   close.classList.add("none");
+    //   modal.classList.add("none");
+    //   modal.innerHTML = "";
+    // }
   }
-}, 1000);
+ }, 1000);
+
+function nextImg() {
+  if (screen.width <= 580) {
+    if (indexImg < 3) {
+      indexImg = ++indexImg;
+    }
+
+    setInterval(() => {
+      if (indexImg === 0) {
+        imgPrev.src = "./images/image-product-1.jpg";
+      } else if (indexImg === 1) {
+        imgPrev.src = "./images/image-product-2.jpg";
+      } else if (indexImg === 2) {
+        imgPrev.src = "./images/image-product-3.jpg";
+      } else if (indexImg === 3) {
+        imgPrev.src = "./images/image-product-4.jpg";
+      }
+    }, 500);
+  }
+}
+
+function previousImg() {
+  if (indexImg !== 0) {
+    indexImg = indexImg - 1;
+  }
+}
 
 function addToCard() {
   if (item === 0) {
@@ -137,4 +170,14 @@ function delCart() {
 
 function opentCart() {
   closeCart.classList.toggle("cartOpenOn");
+}
+
+function openMenuMobile() {
+  menu.classList.add("menu_open");
+  modal.classList.add("open");
+}
+
+function closeMenu() {
+  menu.classList.remove("menu_open");
+  modal.classList.remove("open");
 }
